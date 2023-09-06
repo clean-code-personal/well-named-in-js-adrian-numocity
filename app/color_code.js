3a9b42de2040aebd71944487a2de83d78bccec80
adrian-numocity <adrian.p@numocity.com>
2023-09-06 07:29:07 +0530

    Tested Code

---
+++ app/color_code.js
@@ -0,0 +1,7 @@
+const MajorColorNames = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+const MinorColorNames = [
+    "BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
+module.exports = {MajorColorNames,MinorColorNames};
